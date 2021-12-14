731fcced74c37c8da5e3876ed8e360f9560d4898
Soila Pertet <nicolepertet@gmail.com>
2021-12-14 09:57:53 +0300

    Add an alert to the function if year is not entered

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 function leapYear() {
-  var year = document.getElementById("leapYear").value; 
-  if(year % 4 !== 0) {
+  var year = document.getElementById("leapYear").value;
+  if (year = " ") {
+    alert("Year not entered!")
+  }
+  else if(year % 4 !== 0) {
     document.getElementById("output").innerHTML = (""+year+" is NOT a leap year.");
   }
   else if (year % 100 === 0 && year % 400 === 0) {
