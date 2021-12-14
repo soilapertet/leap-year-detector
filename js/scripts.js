282a2f945a03cd426c54637be2be20b450e0e15d
Soila Pertet <nicolepertet@gmail.com>
2021-12-14 08:52:09 +0300

    Create a function which evaluates and outputs if a year is a leap year

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function leapYear() {
+  var year = document.getElementById("leapYear").value; 
+  if(year % 4 !== 0) {
+    document.getElementById("output").innerHTML = (""+year+" is NOT a leap year.");
+  }
+  else if (year % 4 === 0) {
+    document.getElementById("output").innerHTML = (""+year+" is a leap year!"); 
+  }
+  else if (year % 100 === 0 && year % 400 === 0) {
+    document.getElementById("output").innerHTML = (""+year+" is a leap year!");
+  }
+  else if (year % 100 === 0) {
+    document.getElementById("output").innerHTML = (""+year+" is NOT a leap year.");
+  }
+  else {
+    return false;
+  }
+}
